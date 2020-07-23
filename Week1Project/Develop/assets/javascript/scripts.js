a6f527112a5c72f20bacbfccb1091238b66b184b
Isatou D Sanyang <isasanyang1986@yahoo.co.uk>
2020-07-23 16:07:24 -0500

    Remove week1folder

--- Week1Project/Develop/assets/javascript/scripts.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myHeader");
-    if (x.className === "header") {
-      x.className += " responsive";
-    } else {
-      x.className = "header";
-    }
-  }
